aac493138a3996b20a601ef8bc431047a5d5d98f
Edson Carvalho Inturia <30557202+edsoncarvalhointuria@users.noreply.github.com>
2025-03-22 02:10:11 -0300

    atualizando click link

--- src/components/Projeto.jsx
+++ src/components/Projeto.jsx
@@ -18,7 +18,6 @@ function Projeto({ img, titulo, desc, alt, link }) {
             setClick(true);
 
             const start = (ev) => {
-                setClick(true);
                 item.current.style.left = ev.clientX - (posX - left) + "px";
                 item.current.style.top = ev.clientY - (posY - top) + "px";
             };
